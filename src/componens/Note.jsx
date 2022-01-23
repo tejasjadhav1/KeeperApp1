19dbd540877a8139c4690415ce18c8ec8064c067
tejasjadhav1 <74096170+tejasjadhav1@users.noreply.github.com>
2022-01-24 03:37:01 +0530

    Initial commit

---
+++ src/componens/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <note className="note">
+      <h1>This is the title</h1>
+      <p>This is the content</p>
+    </note>
+  );
+}
+
+export default Note;
